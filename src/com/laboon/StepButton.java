5dcf14d65d3a4d7c1ebf13bf49938432ef10bd55
becca@addison.us <becca@addison.us>
2017-04-03 15:27:50 -0400

    linting for RunButton

--- src/com/laboon/StepButton.java
+++ src/com/laboon/StepButton.java
@@ -1,13 +1,17 @@
 package com.laboon;
 
 import java.awt.*;
-import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class StepButton extends JButton {
 
     private MainPanel _m;
     
+    /**
+     * Constructor of StepButton
+     * @param m MainPanel that StepButton is in
+     */
     public StepButton(MainPanel m) {
 	super("Step");
 	_m = m;
